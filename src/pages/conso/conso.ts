c6d52bef7a7d9c948d96ec892005b5384b0dbe5f
STE0038130 <ldengremont@auchan.fr>
2017-06-28 22:55:19 +0200

    feat: localStorage

--- src/pages/conso/conso.ts
+++ src/pages/conso/conso.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,5 +7,42 @@ import { NavController } from 'ionic-angular';
 })
 export class ConsoPage {
 
+  date: String;
+  inventory: any[];
+
+
   constructor(public navCtrl: NavController) { }
+
+  ngOnInit(){
+
+    localStorage.setItem('TotalKCal', '0');
+    localStorage.setItem('TotalKCalStart', '14-02-2017');
+    localStorage.setItem('TotalKCalWeek', '0');
+    localStorage.setItem('TotalKCalWeekStart', '22-06-2017');
+    localStorage.setItem('TotalKCalDay', '0');
+    localStorage.setItem('TotalKCalDayStart', '29-06-2017');
+    localStorage.setItem('TotalKCalLoose', '0');
+    localStorage.setItem('TotalKCalWeekLoose', '0');
+    localStorage.setItem('TotalKCalDayLoose', '0');
+
+    this.date = new Date().toISOString();
+    console.log(this.date)
+    this.inventory = [
+      {
+        cug: '573891',
+        stock: '1'
+      },
+      {
+        cug: '10087',
+        stock: '1'
+      },
+      {
+        cug: '86363',
+        stock: '1'
+      }
+    ]
+  }
+
+
+
 }
