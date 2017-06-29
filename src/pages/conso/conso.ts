97582dc8275543ccb7632b0516985653cc308a74
STE0038130 <ldengremont@auchan.fr>
2017-06-29 02:57:14 +0200

    feat(conso): it's work

--- src/pages/conso/conso.ts
+++ src/pages/conso/conso.ts
@@ -9,29 +9,75 @@ export class ConsoPage {
 
   date: String;
   inventory: any[];
+  itemsEat: any[];
+  TotalKCalDay: number;
 
 
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController) {
+  }
 
-  ngOnInit(){
+  ngOnInit() {
+
+    this.TotalKCalDay = parseInt(localStorage.getItem('TotalKCalDay'));
 
     this.date = new Date().toISOString();
+    console.log(this.date)
     this.inventory = [
       {
-        cug: '573891',
-        stock: '1'
+        cug: '80659',
+        name: 'Pain au chocolat',
+        cal: '400',
+        count: '3'
+      },
+      {
+        cug: '11974',
+        name: 'Pizza',
+        cal: '266',
+        count: '2'
+      },
+      {
+        cug: '12095',
+        name: 'Pizza',
+        cal: '282',
+        count: '1'
+      },
+      {
+        cug: '141418',
+        name: 'Coca',
+        cal: '38',
+        count: '5'
       },
       {
         cug: '10087',
-        stock: '1'
+        name: 'Carottes rapées',
+        cal: '29',
+        count: '1'
       },
       {
-        cug: '86363',
-        stock: '1'
+        cug: '863632',
+        name: 'Taboulé',
+        cal: '160',
+        count: '1'
+      },
+      {
+        cug: '243954',
+        name: 'Tarte aux pommes',
+        cal: '237',
+        count: '8'
       }
     ]
+    this.itemsEat = []
   }
 
-
+  eatItem(index){
+    console.log(index);
+    this.itemsEat.push(this.inventory[index]);
+    this.inventory[index].count--;
+    this.TotalKCalDay += parseInt(this.inventory[index].cal);
+    if (!this.inventory[index].count) {
+      this.inventory.splice(index, 1);
+    }
+    localStorage.setItem('TotalKCalDay', this.TotalKCalDay.toString());
+  }
 
 }
